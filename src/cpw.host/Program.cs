b9bc2f809495b77dfe0845d60ff8036fc624799f
spatten <stephenpatten@gmail.com>
2016-11-26 08:24:29 -0800

    mutex

---
+++ src/cpw.host/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cpw.lib;
+
+namespace cpw.host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start");
+            try
+            {
+                var mutex = new MutexTest();
+                mutex.CriticalRegionTest1();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            Console.WriteLine("finish");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/cpw.lib/MutexTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace cpw.lib
+{
+    public class MutexTest
+    {
+        //Signaling and waiting
+
+        //Signaled
+
+        //Nonsignaled
+
+        public void CriticalRegionTest1()
+        {
+            //Create a mutex which is in the signaled state
+            Mutex mutant = new Mutex();
+
+            mutant.WaitOne();
+            try
+            {
+                //Critical Region
+                
+            }
+            finally
+            {
+                mutant.ReleaseMutex();
+            }
+
+            mutant.Close();
+        }
+
+    }
+}
